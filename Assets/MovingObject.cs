313c5c7ebe0429933b93ac527050074da3e059ec
cherebas <162827421+cherebas@users.noreply.github.com>
2024-03-10 14:03:42 +0300

    Скрипт имитации движения деревьев и машин

--- Assets/MovingObject.cs
+++ Assets/MovingObject.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MovingObject : MonoBehaviour
 {
     public float speed = 100;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public float resetDestination = -30f;
+    public float resetZPosition = 0f;
+    public bool destroyOnReset = false;
+    private void Update()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y,
             transform.position.z - speed * Time.deltaTime);
+
+        if (!(transform.position.z <= resetDestination)) return;
+        
+        if (destroyOnReset)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, resetZPosition);
     }
 }
